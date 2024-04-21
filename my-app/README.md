39ba7ecd48df1a68c8323944f2f156bfe7525d0f
mirohi <m_hintikka@hotmail.com>
2024-04-21 11:06:26 +0300

    Add README.md

--- my-app/README.md
+++ my-app/README.md
@@ -1,3 +1,4 @@
 ## First component assignment
 
-[Link to public/html]()
+[Link to public/html](https://users.metropolia.fi/~mirohi/WebOhjelmointi/Week5/First_component/dist/)
+
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/~mirohi/WebOhjelmointi/Week5/First_component/dist/",
+});
